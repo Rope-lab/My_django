4817785fa936fbcd659dbe6c84bed78377685acb
Roger Petersson <81437090+Rope-lab@users.noreply.github.com>
2021-05-24 15:34:45 +0200

    Update views.py

--- App_Two/views.py
+++ App_Two/views.py
@@ -5,5 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<em>My second app!</em>')
 def help(request):
+    my_dict = {"insert_help":"Hello from views!"}
+    return render(request,"help.html",context=my_dict)
     
 
