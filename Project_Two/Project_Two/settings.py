7ccd1526eef3a94f3056a0317309b27d74b17082
Roger Petersson <81437090+Rope-lab@users.noreply.github.com>
2021-05-25 10:56:18 +0200

    Update settings.py

--- Project_Two/Project_Two/settings.py
+++ Project_Two/Project_Two/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')ex3b4^&r^$^$a#d9*@iy*g#x70e^4*-l$(j5yqw^i$&zn%zvq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
