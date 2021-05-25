337f2085ece04bf284e62aaaf4c6904ad880e177
Roger Petersson <ropethepope@gmail.com>
2021-05-25 10:54:21 +0200

    Models and more

--- Project_Two/App_Two/views.py
+++ Project_Two/App_Two/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<em>My second app!</em>')
+def help(request):
+    my_dict = {"insert_help":"Hello from views!"}
+    return render(request,"help.html",context=my_dict)
