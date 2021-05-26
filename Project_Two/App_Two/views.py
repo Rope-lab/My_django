8e6778c70fb74bf2074ca26fd524e647036dad20
Roger Petersson <ropethepope@gmail.com>
2021-05-26 15:31:40 +0200

    Displayed data in template index

--- Project_Two/App_Two/views.py
+++ Project_Two/App_Two/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from App_Two.models import Webpage, AccessRecord 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<em>My second app!</em>')
-    return render(request,"App_Two_templates\index.html")
+    #return HttpResponse('<em>My second app!</em>')
+    webpages = Webpage.objects.all().values('topic__top_name','name','url','accessrecord__date')#django will automaticly look up the class related to the column
+    my_dict = {
+        'wbp': webpages,
+    }
+    return render(request,"App_Two_templates\index.html",my_dict)
 def help(request):
     my_dict = {"insert_help":"Hello from views!"}
     return render(request,"App_Two_templates\help.html",context=my_dict)
